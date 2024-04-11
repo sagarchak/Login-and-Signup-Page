fc818e2a9303a0aac91ac7e72298f3c9febe72de
sagarchak <166717342+sagarchak@users.noreply.github.com>
2024-04-11 19:48:42 +0530

    new_test

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
     return GraphQLProvider(
         client: client,
         child: MaterialApp(
+            debugShowCheckedModeBanner: false,
             title: 'Flutter Demo',
             theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
